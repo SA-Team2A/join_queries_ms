102726e0b1ec1b4b00846c72bd43da29c6450fad
Raúl Coral <rfcoralj@unal.edu.co>
2019-02-14 19:56:45 -0500

    Fix res when no comments

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 })
 
 app.get('/users_comments', (req, res, next) => {
-  const recipe_id = parseInt(req.query.recipe_id)
+  const recipe_id = req.query.recipe_id
   comments.find({ recipe_id }, (err, result) => {
     if (err) {
       console.log(err)
@@ -31,21 +31,19 @@ app.get('/users_comments', (req, res, next) => {
     }
     if (result.length === 0) {
       console.log(`No comments to recipe with id = ${recipe_id}`)
-      return res.status(404).send({
-        code: 404,
-        message: 'NOT FOUND'
+      res.status(200).json({})
+    } else {
+      users.where_in(result.map( r => r.user_id ), (err2, result2) => {
+        if (err2) {
+          console.log(err2)
+          return res.status(500).send({
+            code: 500,
+            message: 'INTERNAL ERROR'
+          })
+        }
+        res.status(200).send(join(result, 'user_id', result2, 'id'))
       })
     }
-    users.where_in(result.map( r => r.user_id ), (err2, result2) => {
-      if (err2) {
-        console.log(err2)
-        return res.status(500).send({
-          code: 500,
-          message: 'INTERNAL ERROR'
-        })
-      }
-      res.status(200).send(join(result, 'user_id', result2, 'id'))
-    })
   })
 })
 
